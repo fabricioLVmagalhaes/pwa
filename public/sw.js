bfd7d91816c493876d10f3c33dde1d2ddc8af140
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2020-05-03 19:47:57 -0300

    Adding Cache Versioning

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static').then(function (cache) {
+    caches.open('static-v3').then(function (cache) {
       console.log('[Service Worker] Precaching App Shell');
       cache.addAll([
         '/',
@@ -20,11 +20,11 @@ self.addEventListener('install', function (event) {
     })
   );
 });
-
 self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
+
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
@@ -40,6 +40,9 @@ self.addEventListener('fetch', function (event) {
               cache.put(event.request.url, res.clone());
               return res;
             })
+        })
+        .catch(function(err){
+          console.log(err);          
         });
       }
     })
