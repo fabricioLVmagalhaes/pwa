baff3b3c22efd6ea0dbaee0d11ab49d14ffb777e
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2020-05-03 20:14:45 -0300

    Different Cache Versions & Cleanup

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static-v3').then(function (cache) {
+    caches.open('static-v2').then(function (cache) {
       console.log('[Service Worker] Precaching App Shell');
       cache.addAll([
         '/',
@@ -9,41 +9,49 @@ self.addEventListener('install', function (event) {
         '/src/js/app.js',
         '/src/js/promise.js',
         '/src/js/fetch.js',
+        '/src/js/feed.js',
         '/src/js/material.min.js',
         '/src/css/app.css',
         '/src/css/feed.css',
         '/src/images/main-image.jpg',
         'https://fonts.googleapis.com/css?family=Roboto:400,700',
         'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
       ]);
     })
   );
 });
 self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function(key){
+        if(key !== 'static-v2' && key !== 'dynamic') {
+          console.log('[Service Worker] Removing old cache.', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
   return self.clients.claim();
 });
 
-
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(function (response) {
+    caches.match(event.request).then(function (response) {
       if (response) {
         return response;
       } else {
         return fetch(event.request)
-        .then(function(res){
-          return caches.open('dynamic')
-            .then(function(cache){
+          .then(function (res) {
+            return caches.open('dynamic').then(function (cache) {
               cache.put(event.request.url, res.clone());
               return res;
-            })
-        })
-        .catch(function(err){
-          console.log(err);          
-        });
+            });
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
       }
     })
   );
