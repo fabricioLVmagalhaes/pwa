7559f006d90e8353fb9222dbb81dbe58e018dbdb
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2020-05-04 21:38:08 -0300

    adding a cache only strategy for static cache

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,28 @@
 var CACHE_STATIC_VERSION = 'static-v14';
 var CACHE_DINAMIC_NAME = 'dynamic-v3';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
 
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open(CACHE_STATIC_VERSION).then(function (cache) {
       console.log('[Service Worker] Precaching App Shell');
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/offline.html',
-        '/src/js/app.js',
-        '/src/js/promise.js',
-        '/src/js/fetch.js',
-        '/src/js/feed.js',
-        '/src/js/material.min.js',
-        '/src/css/app.css',
-        '/src/css/feed.css',
-        '/src/images/main-image.jpg',
-        'https://fonts.googleapis.com/css?family=Roboto:400,700',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-      ]);
+      cache.addAll(STATIC_FILES);
     })
   );
 });
@@ -42,6 +43,18 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
+function isInArray(string, array) {
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) {
+    // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
+  }
+  return array.indexOf(cachePath) > -1;
+}
+
 self.addEventListener('fetch', function (event) {
   var url = 'https://httpbin.org/get';
   if (event.request.url.indexOf(url) > -1) {
@@ -52,6 +65,10 @@ self.addEventListener('fetch', function (event) {
           return res;
         });
       })
+    );
+  }else if (isInArray(event.request.url, STATIC_FILES)) {
+    event.respondWith(
+      caches.match(event.request)
     );
   } else {
     event.respondWith(
@@ -68,9 +85,9 @@ self.addEventListener('fetch', function (event) {
             })
             .catch(function (err) {
               return caches.open(CACHE_STATIC_VERSION).then(function (cache) {
-                if(event.request.url.indexOf('/help')) {
+                if (event.request.url.indexOf('/help')) {
                   return cache.match('/offline.html');
-                }               
+                }
               });
             });
         }
